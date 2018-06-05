b73b9f05bb9bd019ceebc2166427592231600186
Tyler Westlie <tyler.westlie@gmail.com>
2018-06-05 10:36:45 -0600

    visitor sees movie rating on movie show page

--- spec/features/visitor/vistor_sees_movie_show_page_spec.rb
+++ spec/features/visitor/vistor_sees_movie_show_page_spec.rb
@@ -6,13 +6,24 @@ describe 'visitor visits movie show page' do
     genre1 = Genre.create(name: 'action')
     genre2 = Genre.create(name: 'romance')
     movie1 = genre1.movies.create(title: 'Movie One', description: 'great film', rating: 5, director_id: director1.id)
-    a = MovieGenre.create(movie_id: movie1.id, genre_id: genre2.id)
-    # require 'pry'; binding.pry
+    MovieGenre.create(movie_id: movie1.id, genre_id: genre2.id)
 
     visit movie_path(movie1.slug)
 
-
+    expect(page).to have_content("Genres for this Movie:")
     expect(page).to have_content(genre1.name)
     expect(page).to have_content(genre2.name)
+  end
+
+  it 'can see all the genres associated with a movie' do
+    director1 = Director.create(name:'Bob')
+    genre1 = Genre.create(name: 'action')
+    genre2 = Genre.create(name: 'romance')
+    movie1 = genre1.movies.create(title: 'Movie One', description: 'great film', rating: 5, director_id: director1.id)
+    MovieGenre.create(movie_id: movie1.id, genre_id: genre2.id)
+
+    visit movie_path(movie1.slug)
+
+    expect(page).to have_content(movie1.rating)
   end
 end
